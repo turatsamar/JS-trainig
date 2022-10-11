a42a47130485ad5c8d4fcdb349028269e17603f8
TIMA <64804931+turatsamar@users.noreply.github.com>
2022-10-12 03:52:12 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+let f='Turatbek';
+console.log(f);
+let d='+';
+console.log(d);
+let a='Rita';
+console.log(a);
+let k='='
+console.log(k);
+let m='Sofiya';
+console.log(m);
+const o='Rita+Turat=Sofiya'
+console.log(o);
+let q
+console.log(q);
+{
+    m=1995,
+    r=1991
+    s=2022
+}
+
+
+console.log(m);
+
